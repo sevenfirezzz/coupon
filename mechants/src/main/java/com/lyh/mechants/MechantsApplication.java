22a3eafe7afcd9a791c1d8c0fa5762117d426bd5
liyuhui <liyuhui@liyuhuideMacBook-Air.local>
2018-11-22 08:05:47 -0800

    init mechants modules

---
+++ mechants/src/main/java/com/lyh/mechants/MechantsApplication.java
@@ -0,0 +1,14 @@
+package com.lyh.mechants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechantsApplication.class, args);
+    }
+
+
+}
